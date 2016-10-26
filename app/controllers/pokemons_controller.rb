ad02925b3316d2c8583a049df59a8c9417ca9649
Link Arneson <linkarneson@gmail.com>
2016-10-26 15:12:17 -0700

    bare min for proj 1 finished

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,6 +3,30 @@ class PokemonsController < ApplicationController
     pokemon = Pokemon.find(params[:id])
     pokemon.trainer_id = current_trainer.id
     pokemon.save
-    redirect_to "/"
+    redirect_to root_path
+  end
+
+  def damage
+    pokemon = Pokemon.find(params[:id])
+    pokemon.health -= 10
+    pokemon.save
+    if pokemon.health <= 0
+      pokemon.destroy
+    end
+    redirect_to trainer_path(id: params[:trainer])
+  end
+
+  def new
+  end
+
+  def create
+    pokemon = Pokemon.new(:name => params[:pokemon][:name], :health => 100, :level => 1, :trainer_id => current_trainer.id)
+    if !pokemon.valid?
+      redirect_to new_pokemon_path
+      flash[:error] = pokemon.errors.full_messages.to_sentence
+    else
+      pokemon.save
+      redirect_to trainer_path(id: current_trainer.id)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :trainers
   resources :trainers
   patch '/capture/:id', to: 'pokemons#capture', as: 'capture'
+  patch '/damage/:id', to: 'pokemons#damage', as: 'damage'
+  get 'pokemons/new', to: 'pokemons#new', as: 'new_pokemon'
+  post 'pokemons/new', to: 'pokemons#create', as: 'pokemons'
+  #resources :pokemons
 end
